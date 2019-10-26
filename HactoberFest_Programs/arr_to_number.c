3c0956a1b9e380b1befea8cbab27ccb24d3ad2f7
Anarghyau <57026591+Anarghyau@users.noreply.github.com>
2019-10-26 01:30:06 -0400

    Update arr_to_number.c
    reading value for dig

--- HactoberFest_Programs/arr_to_number.c
+++ HactoberFest_Programs/arr_to_number.c
@@ -3,12 +3,14 @@
 #include<math.h>
 int main(){
 	int i,dig,number=0;
+	printf("enter the value:");
+	scanf("%d",&dig);
 	//Read a number to complete the program.
 	int arr[dig];
 	for(i=0;i<dig;i++){
 		printf("\nEnter %dth digit: ",i+1);
 		scanf("%d",&arr[i]);
-		number=number+arr[i]*pow(10,i);
+		number=number+arr[i]*math.pow(10,i);
 	}
 	printf("\nThe number is: %d\n",number);
 return 0;
